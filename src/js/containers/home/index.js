24a5a2bc59369e9f2d3e9da12d97051b56e2edfb
Nadia Iqbal <nadee.shocks@gmail.com>
2019-05-09 13:56:20 +0500

    corrected catagories bug

--- src/js/containers/home/index.js
+++ src/js/containers/home/index.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { firestoreConnect } from 'react-redux-firebase';
-import {compose } from 'redux';
+import { compose } from 'redux';
+import CatagoryList from '../../components/catagoryList/index';
 
 class Home extends Component {
-  render(){
-    return(
+  render() {
+    const { catagories } = this.props;
+    return (
       // projects && projects.map();
       // React, Redux & Firebase App Tutorial #20 - Project Details Data for Rent Gear.
-      
-      <h1>Home</h1>
-    )
+      <div className="home">
+        <div className="home-head">
+          <h1>Home</h1>
+        </div>
+        <div className="home-body">
+          <CatagoryList catagories={catagories} />
+        </div>
+      </div>
+    );
   }
 }
 
 const mapStateToProps = (state) => {
   return {
+    catagories: state.firestore.ordered.catagories
   }
 }
 export default compose(
   firestoreConnect([
-    { collection : 'catagories'}
+    { collection: 'catagories' }
   ]),
   connect(mapStateToProps)
 )(Home);
